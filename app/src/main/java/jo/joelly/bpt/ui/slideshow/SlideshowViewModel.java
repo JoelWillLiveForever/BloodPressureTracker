d5b23d80fb82605b782839e014847abaf053e3c9
Vladimir Nikulin <mail.jorey@gmail.com>
2023-12-06 21:25:31 +0300

    Initial commit

---
+++ app/src/main/java/jo/joelly/bpt/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package jo.joelly.bpt.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
